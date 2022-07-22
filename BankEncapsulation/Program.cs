a2640061605c1c178c49de837ae0c9b414fda260
Diana Mendoza <diana.pat.mendoza@gmail.com>
2022-07-21 22:04:19 -0500

    initial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dianasAccount = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            dianasAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you! Your balance is now ${dianasAccount.GetBalance()}");
+        }
+    }
+}
+
